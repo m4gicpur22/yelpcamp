f252be234c6001f23b968afd252dad8c4c57d827
DESKTOP-LLQOSHI\Affner Lefevre <m4gictv@gmail.com>
2019-01-12 00:35:23 -0500

    First Haskell Assignment

---
+++ Documents/COP4020/Haskell/Listcomp.hs
@@ -0,0 +1,10 @@
+import Data.List
+import System.IO
+
+
+multiplesOf5 n = [x | x <- [1..n], x `mod` 5 == 0]
+
+multiplesOf7Or11 n = [x | x <- [1..n], x `mod` 7 == 0 || x `mod` 11 == 0]
+
+multiplesOf3And7 n = [x | x <- [1..n], x `mod` 3 == 0, x `mod` 7 == 0]gg
+
